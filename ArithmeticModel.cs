7523fe784bfcbf83d9c93b19ce2de0ac8d92afe2
parthh-111 <parthnagwekar2006@gmail.com>
2026-07-10 10:22:15 +0530

    Add files via upload

---
+++ ArithmeticModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArithmeticMVC.Models
+{
+    public class ArithmeticModel
+    {
+        [Required]
+        [Display(Name = "First Number")]
+        public double Number1 { get; set; }
+
+        [Required]
+        [Display(Name = "SEcond Number")]
+        public double Number2 { get; set; }
+
+        public string Opertaion { get; set; } = " ";
+        public double Result { get; set; }
+        public string ErrorMessage { get; set; } = "";
+    }
+}
